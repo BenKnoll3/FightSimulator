6b4a2e189580700f4e1a442bf8679077f7e821aa
Ben Knoll <bkbighorn@gmail.com>
2022-12-18 00:38:48 -0800

    Updated fighter traits and winner logic

--- logic.js
+++ logic.js
@@ -1,10 +1,9 @@
-let wre1, wre2;
-let str1, str2;
-let gra1, gra2;
-let car1, car2;
+let hei1, hei2;
+let bui1, bui2;
+let pow1, pow2;
+let chi1, chi2;
+let sub1, sub2;
 const hdc = heightdrop-content;
-
-
 
 function showDrop(p1) {
     document.getElementById(p1).classList.toggle("show");
@@ -14,65 +13,40 @@ function hideDrop(p1) {
     document.getElementById(p1).classList.toggle("hide");
 }
 
-function setBeginner(skill, headId) {
-
-    document.getElementById(headId).innerHTML = "Beginner";
-    if (skill === 1){
-        wre1 = 1;
-    } else if (skill === 2){
-        str1 = 1;
-    } else if (skill === 3){
-        gra1 = 1;
-    } else if (skill === 4){
-        wre2 = 1;
-    } else if (skill === 5){
-        str2 = 1;
-    } else if (skill === 6){
-        gra2 = 1;
-    } 
-
+function setPlayer1(points, headId, trait, traitNumber) {
+    document.getElementById(headId).innerHTML = trait;
+    if (traitNumber === 1){
+        hei1 = points;
+    } else if (traitNumber === 2){
+        bui1 = points;
+    } else if (traitNumber === 3){
+        pow1 = points;
+    } else if (traitNumber === 4){
+        chi1 = points;
+    } else if (traitNumber === 5){
+        sub1 = points;
+    }
 }
-
-function setIntermediate(skill, headId) {
-    document.getElementById(headId).innerHTML = "Intermediate";
-    if (skill === 1){
-        wre1 = 2;
-    } else if (skill === 2){
-        str1 = 2;
-    } else if (skill === 3){
-        gra1 = 2;
-    } else if (skill === 4){
-        wre2 = 2;
-    } else if (skill === 5){
-        str2 = 2;
-    } else if (skill === 6){
-        gra2 = 2;
-    } 
-}
-
-function setExpert(skill, headId) {
-    document.getElementById(headId).innerHTML = "Expert";
-    if (skill === 1){
-        wre1 = 3;
-    } else if (skill === 2){
-        str1 = 3;
-    } else if (skill === 3){
-        gra1 = 3;
-    } else if (skill === 4){
-        wre2 = 3;
-    } else if (skill === 5){
-        str2 = 3;
-    } else if (skill === 6){
-        gra2 = 3;
-    } 
+function setPlayer2(points, headId, trait, traitNumber) {
+    document.getElementById(headId).innerHTML = trait;
+    if (traitNumber === 1){
+        hei2 = points;
+    } else if (traitNumber === 2){
+        bui2 = points;
+    } else if (traitNumber === 3){
+        pow2 = points;
+    } else if (traitNumber === 4){
+        chi2 = points;
+    } else if (traitNumber === 5){
+        sub2 = points;
+    }
+    //alert(hei2 + ""+bui2 + ""+pow2 + ""+chi2 + ""+sub2 + "");
 }
 
 function faceOff() {
-    if (wre1 + str1 + gra1 > wre2 + str2 + gra2){
+    if (hei1 + bui1 + pow1 + chi1 + sub1 > hei2 + bui2 + pow2 + chi2 + sub2){
         alert("fighter 1 wins!");
     } else {
         alert("fighter 2 wins!");
     }
-
-    
 }
